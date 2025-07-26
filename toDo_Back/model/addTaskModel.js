4a92fef6e1f34270f707f2a8d64c4908b539880c
Sultan Omar <90979782+Sultanomar0013@users.noreply.github.com>
2025-07-26 12:23:36 +0600

    Create addTaskModel.js

---
+++ toDo_Back/model/addTaskModel.js
@@ -0,0 +1,21 @@
+// models/Task.js
+
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  dueDate: { type: Date },
+  status: {
+    type: String,
+    enum: ["Pending", "Completed"],
+    default: "Pending"
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Low"
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Task", TaskSchema);
